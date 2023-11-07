aebf251a5713debaa3e8e50b360835f44994893d
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-11-08 03:19:31 +0900

    * add ranking page API * add queryDSL

--- src/main/java/com/gamza/gomoku/repository/PostRepository.java
+++ src/main/java/com/gamza/gomoku/repository/PostRepository.java
@@ -1,9 +1,6 @@
 package com.gamza.gomoku.repository;
 
 import com.gamza.gomoku.entity.PostEntity;
-import com.gamza.gomoku.entity.UserEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
